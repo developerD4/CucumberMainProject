d999d033334148510ab6bcd277dda863fdbf03cf
developerD4 <dharanitech456@gmail.com>
2025-10-24 12:44:52 +0530

    simple print statement

--- src/test/java/com/utils/TestBase.java
+++ src/test/java/com/utils/TestBase.java
@@ -17,6 +17,7 @@ public class TestBase {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get(baseURL);
+		System.out.println("Opening");
 	}
 	@After
 	public void tearDown() {
